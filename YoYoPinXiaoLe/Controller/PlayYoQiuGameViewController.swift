07903dad7ac59da673bbfc74a8576ec8aec7ca37
Aaron_xu <xuzhou@gozap.com>
2021-05-07 16:29:40 +0800

    bak

--- YoYoPinXiaoLe/Controller/PlayYoQiuGameViewController.swift
+++ YoYoPinXiaoLe/Controller/PlayYoQiuGameViewController.swift
@@ -125,7 +125,6 @@ class PlayYoQiuGameViewController: UIViewController {
         }
 //        self.cancelBtn.addTarget(self.matrix, action: #selector(matrix.cancelAction(_:)), for: .touchUpInside)
 //        self.okBtn.addTarget(self.matrix, action: #selector(matrix.okAction(_:)), for: .touchUpInside)
-        
         self.cancelBtn.addTarget(self, action: #selector(quit), for: .touchUpInside)
         self.okBtn.addTarget(self, action: #selector(undo), for: .touchUpInside)
         
