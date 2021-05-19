4478918f2bbed7621de7b2b2da060c0c238c7c02
Aaron_xu <xuzhou@gozap.com>
2021-05-19 10:37:31 +0800

    优化

--- YoYoPinXiaoLe/View/WithoutAuthorizationView.swift
+++ YoYoPinXiaoLe/View/WithoutAuthorizationView.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class WithoutAuthorizationView: UIView {
-
+    
+    let ruleLabel:UILabel = {
+        let label = UILabel()
+        label.font = blodFontWithSize(13)
+        label.textColor = UIColor(named: "color_title_black")
+        label.numberOfLines = 0
+        label.text = NSLocalizedString("helpCenterRule")
+        return label
+    }()
+    
+    let titleLabel:UILabel = {
+        let label = UILabel()
+        label.font = blodFontWithSize(13)
+        label.textColor = UIColor(named: "color_title_black")
+        label.numberOfLines = 0
+        label.text = NSLocalizedString("helpCenterTil")
+        return label
+    }()
+    
     let oneLabel:UILabel = {
         let label = UILabel()
-        label.font = blodFontWithSize(16)
+        label.font = blodFontWithSize(13)
         label.textColor = UIColor(named: "color_title_black")
         label.numberOfLines = 0
         label.text = NSLocalizedString("helpCenterSum1")
@@ -28,7 +46,7 @@ class WithoutAuthorizationView: UIView {
     
     let twoLabel:UILabel = {
         let label = UILabel()
-        label.font = blodFontWithSize(16)
+        label.font = blodFontWithSize(13)
         label.textColor = UIColor(named: "color_title_black")
         label.numberOfLines = 0
         label.text = NSLocalizedString("helpCenterSum2")
@@ -45,7 +63,7 @@ class WithoutAuthorizationView: UIView {
     
     let threeLabel:UILabel = {
         let label = UILabel()
-        label.font = blodFontWithSize(16)
+        label.font = blodFontWithSize(13)
         label.textColor = UIColor(named: "color_title_black")
         label.numberOfLines = 0
         label.text = NSLocalizedString("helpCenterSum3")
@@ -64,42 +82,56 @@ class WithoutAuthorizationView: UIView {
     
     func sebViews() {
         
+        self.addSubview(ruleLabel)
+        self.addSubview(titleLabel)
+        
         self.addSubview(oneLabel)
         self.addSubview(oneImageView)
         self.addSubview(twoLabel)
         self.addSubview(twoImageView)
         self.addSubview(threeLabel)
         
+        ruleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalToSuperview().offset(5)
+        }
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalTo(ruleLabel.snp.bottom).offset(10)
+        }
+        
         oneLabel.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(25)
-            make.right.equalToSuperview().offset(-25)
-            make.top.equalToSuperview().offset(25)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
         }
         
         oneImageView.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(25)
-            make.right.equalToSuperview().offset(-25)
-            make.top.equalTo(oneLabel.snp.bottom).offset(10)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalTo(oneLabel.snp.bottom).offset(5)
             make.height.equalTo((kScreenWidth - 50) / 2.36)
         }
         
         twoLabel.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(25)
-            make.right.equalToSuperview().offset(-25)
-            make.top.equalTo(oneImageView.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalTo(oneImageView.snp.bottom).offset(10)
         }
         
         twoImageView.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(25)
-            make.right.equalToSuperview().offset(-25)
-            make.top.equalTo(twoLabel.snp.bottom).offset(10)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalTo(twoLabel.snp.bottom).offset(5)
             make.height.equalTo((kScreenWidth - 50) / 1.47)
         }
         
         threeLabel.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(25)
-            make.right.equalToSuperview().offset(-25)
-            make.top.equalTo(twoImageView.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.top.equalTo(twoImageView.snp.bottom).offset(10)
         }
         
     }
